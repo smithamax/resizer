4b4c0727bf42db1b4975450fde1e7edaf46b048f
Dominic Smith <dominic.smith@loke.com.au>
2017-09-14 16:50:59 +1000

    Fix fit types

--- resizer/resize.go
+++ resizer/resize.go
@@ -18,7 +18,21 @@ func Resize(img image.Image, w, h int, fit string) image.Image {
 		if w == 0 || h == 0 {
 			return imaging.Resize(img, w, h, defaultFilter)
 		}
-		return imaging.Fit(img, w, h, defaultFilter)
+
+		size := img.Bounds().Size()
+
+		r := float64(w) / float64(h)
+		ir := float64(size.X) / float64(size.Y)
+
+		var nw, nh int
+		if r > ir {
+			nw = int(float64(h) * ir)
+			nh = h
+		} else {
+			nw = w
+			nh = int(float64(w) / ir)
+		}
+		return imaging.Resize(img, nw, nh, defaultFilter)
 
 	case "crop":
 		return imaging.Thumbnail(img, w, h, defaultFilter)
@@ -29,7 +43,23 @@ func Resize(img image.Image, w, h int, fit string) image.Image {
 
 	case "min":
 		size := img.Bounds().Size()
-		return imaging.Thumbnail(img, min(w, size.X), min(h, size.Y), defaultFilter)
+
+		if size.X > w && size.Y > h {
+			return imaging.Thumbnail(img, w, h, defaultFilter)
+		}
+
+		r := float64(w) / float64(h)
+		ir := float64(size.X) / float64(size.Y)
+
+		var nw, nh int
+		if r > ir {
+			nw = size.X
+			nh = int(float64(size.X) / r)
+		} else {
+			nw = int(float64(size.Y) * r)
+			nh = size.Y
+		}
+		return imaging.Thumbnail(img, nw, nh, defaultFilter)
 	}
 	return img
 }
